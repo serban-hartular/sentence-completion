2dba8623981edd7517ab76e07b9dc9c6ccf7aea4
serban-hartular <81067211+serban-hartular@users.noreply.github.com>
2026-03-04 11:57:59 +0200

    synchronized vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:5000',
-        changeOrigin: true
+        changeOrigin: true,
+        secure: false,
       }
     }
   },
